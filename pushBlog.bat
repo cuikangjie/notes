70cb3e0cb17875f1141e68995b9f0d18bc293b3d
cuikangjie <cuikangjie_90@126.com>
2016-12-23 12:34:59 +0800

    更新

---
+++ pushBlog.bat
@@ -0,0 +1,19 @@
+cd ../blog-kin
+
+git add .
+
+git commit -m "更新"
+
+git push origin master
+
+pause
+
+cd ../notes
+
+git add .
+
+git commit -m "更新"
+
+git push origin master
+
+pause
---
+++ 编译.bat
@@ -0,0 +1,6 @@
+pause
+
+cd ..\markdown-html-page\
+
+npm start
+
